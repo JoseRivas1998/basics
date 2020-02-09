825f6fcd2914efb6ec82da86c55fd94e15f66365
Jose Rodriguez Rivas <example@example.com>
2020-02-08 17:03:39 -0800

    Created sample program

---
+++ sample.c
@@ -0,0 +1,27 @@
+#include "sample.h"
+#define SIZE 10
+
+int main() {
+	srand(time(NULL));
+	int *a = malloc(SIZE * sizeof(int));
+	int *b = malloc(SIZE * sizeof(int));
+	int *c = malloc(SIZE * sizeof(int));
+	for (int i = 0; i < SIZE; ++i) {
+		a[i] = rand() % 100;
+		b[i] = rand() % 100;
+	}
+	add(a, b, c, SIZE);
+	for (int i = 0; i < SIZE; ++i) {
+		printf("%d + %d = %d\n", a[i], b[i], c[i]);
+	}
+	free(a);
+	free(b);
+	free(c);
+	return 0;
+}
+
+void add (int *a, int *b, int *c, int N) {
+	for (int i = 0; i < N; ++i) {
+		c[i] = a[i] + b[i];
+	}
+}
---
+++ sample.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void add (int *a, int *b, int *c, int N);
+
